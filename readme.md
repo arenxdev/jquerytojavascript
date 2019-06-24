80de5c7f90cd959890f85450de0d1168681c5169
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-06-24 14:08:48 -0500

    Reusing functions

--- readme.md
+++ readme.md
@@ -250,3 +250,52 @@ Este flujo es la magia que hay detrás de varias librerías y frameworks que nos
 
   load()
 ```
+
+### Realizando funciones
+
+En esta clase vamos a modificar nuestro código que se encarga de renderizar la plantilla de video para volverlo una función que pueda ser reutilizable. Dicha función recibirá dos parámetros, uno para saber sobre qué lista de películas va a iterar y otro para saber en que contenedor va a renderizar las películas.
+
+```javascript
+  const API_URL = 'https://yts.lt/api/v2/list_movies.json'
+
+  const getData = async (url, genre) => {
+    const action = await fetch(`${url}?genre=${genre}`)
+    const data = await action.json()
+    return data
+  }
+
+  const videoItemTemplate = ({ medium_cover_image, title }) => {
+    return (
+    `<div class="primaryPlaylistItem">
+      <div class="primaryPlaylistItem-image">
+        <img src="${medium_cover_image}">
+      </div>
+      <h4 class="primaryPlaylistItem-title">
+        ${title}
+      </h4>
+    </div>`
+    )
+  }
+
+  const putComponent = (lstMovies, element, ) => {
+    let htmlComponent = ""
+    lstMovies.forEach(movie => {
+      const htmlString = videoItemTemplate(movie)
+      htmlComponent += htmlString
+    })
+    element.innerHTML = htmlComponent
+  }
+
+  const renderMovies = async genre => {
+    const apidata = await getData(API_URL, genre)
+    const $container = document.getElementById(genre)
+    putComponent(apidata.data.movies, $container)
+  }
+
+  const load = async () => {
+    const movies = ['action', 'drama', 'animation']
+    movies.forEach(movie => renderMovies(movie))
+  }
+
+  load()
+```
--- src/js/home.js
+++ src/js/home.js
@@ -1,6 +1,10 @@
-/* eslint-disable no-debugger */
-/* eslint-disable no-undef */
 const API_URL = 'https://yts.lt/api/v2/list_movies.json'
+
+const getData = async (url, genre) => {
+  const action = await fetch(`${url}?genre=${genre}`)
+  const data = await action.json()
+  return data
+}
 
 const videoItemTemplate = ({ medium_cover_image, title }) => {
   return (
@@ -15,44 +19,24 @@ const videoItemTemplate = ({ medium_cover_image, title }) => {
   )
 }
 
-const getData = async (url, genre) => {
-  const action = await fetch(`${url}?genre=${genre}`)
-  const data = await action.json()
-  return data
+const putComponent = (lstMovies, element, ) => {
+  let htmlComponent = ""
+  lstMovies.forEach(movie => {
+    const htmlString = videoItemTemplate(movie)
+    htmlComponent += htmlString
+  })
+  element.innerHTML = htmlComponent
+}
+
+const renderMovies = async genre => {
+  const apidata = await getData(API_URL, genre)
+  const $container = document.getElementById(genre)
+  putComponent(apidata.data.movies, $container)
 }
 
 const load = async () => {
-  const dataAction = await getData(API_URL, 'action')
-  // const dataHorror = await getData(API_URL, 'horror')
-  // const dataDrama = await getData(API_URL, 'drama')
-  // const dataAnimation = await getData(API_URL, 'animation')
-  
-  const $actionContainer = document.getElementById('action')
-  
-  dataAction.data.movies.forEach(movie => {
-    const htmlString = videoItemTemplate(movie)
-    const html = document.implementation.createHTMLDocument()
-    html.body.innerHTML = htmlString
-    $actionContainer.append(html.body.children[0])
-  })
+  const movies = ['action', 'drama', 'animation']
+  movies.forEach(movie => renderMovies(movie))
 }
 
 load()
-
-// eslint-disable-next-line no-undef 
-// const $home = $('.home .list #item')
-// const $dramaContainer = document.getElementById('drama')
-// const $animationContainer = document.getElementById('animation')
-
-// const $featuringContainer = document.getElementById('featuring')
-// const $form = document.getElementById('form')
-// const $home = document.getElementById('home')
-
-// const $modal = document.getElementById('modal')
-// const $overlay = document.getElementById('overlay')
-// const $hideModal = document.getElementById('hide-modal')
-
-// const $modalTitle = $modal.querySelector('h1')
-// const $modalImage = $modal.querySelector('img')
-// const $modalDescription = $modal.querySelector('p')
-
